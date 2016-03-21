5fa94a0d10217a4bafef34f7089085169a72d109
D-kevin <kevinclark@foxmail.com>
2016-03-22 00:24:19 +0800

    Add searching  destination function (not fully completed yet)

--- app/src/main/java/com/kevin/futuremeet/fragment/DestChooseFragment.java
+++ app/src/main/java/com/kevin/futuremeet/fragment/DestChooseFragment.java
@@ -3,6 +3,7 @@ package com.kevin.futuremeet.fragment;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
@@ -11,18 +12,34 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
 import com.baidu.location.BDLocation;
 import com.baidu.location.BDLocationListener;
 import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.search.core.PoiInfo;
+import com.baidu.mapapi.search.poi.OnGetPoiSearchResultListener;
+import com.baidu.mapapi.search.poi.PoiCitySearchOption;
+import com.baidu.mapapi.search.poi.PoiDetailResult;
+import com.baidu.mapapi.search.poi.PoiResult;
+import com.baidu.mapapi.search.poi.PoiSearch;
 import com.kevin.futuremeet.R;
 import com.kevin.futuremeet.activity.CityChooseActivity;
 import com.kevin.futuremeet.beans.CurrentLocation;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class DestChooseFragment extends Fragment {
+
+public class DestChooseFragment extends Fragment implements OnGetPoiSearchResultListener{
 
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
@@ -40,6 +57,18 @@ public class DestChooseFragment extends Fragment {
 
     private LocationClient mLocationClient;
     private SharedPreferences mSharedPref;
+    private PoiSearch mPoiSearch;
+    private ListView mSearchResListView;
+    private ArrayList<Map<String,String>> mPoiList;
+    private SimpleAdapter mPoiListAdapter;
+    private SearchView mDestSearchView;
+    private Button mPoiSearchButton;
+
+
+    private static final String POI_NAME="poi_name";
+    private static final String POI_ADDRESS="poi_address";
+
+
 
 
 //    private OnFragmentInteractionListener mListener;
@@ -47,7 +76,6 @@ public class DestChooseFragment extends Fragment {
     public DestChooseFragment() {
         // Required empty public constructor
     }
-
 
     /**
      * Use this factory method to create a new instance of
@@ -82,12 +110,93 @@ public class DestChooseFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_dest_choose, container, false);
         mSharedPref = PreferenceManager.getDefaultSharedPreferences(getActivity());
         initToolbar(root);
-        mLocationClient = new LocationClient(getActivity().getApplicationContext());
-        initLocationOption();
-        mLocationClient.registerLocationListener(myLocationListener);
+        initLocationFunc();
         mLocationClient.start();
+        initPoiSearch();
+        initPoiListView(root);
+        initPoiSearchView(root);
+        initPoiSearchButton(root);
+
         return root;
     }
+
+    private void initPoiSearchView(View root) {
+        mDestSearchView = (SearchView) root.findViewById(R.id.dest_place_searchview);
+        mDestSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (newText.length()>0){
+                    mPoiSearchButton.setEnabled(true);
+                }else{//when the searchview is empty the listview should be empty too
+                    mPoiSearchButton.setEnabled(false);
+                    if (mPoiList.size()>0){
+                        mPoiList.clear();
+                        mPoiListAdapter.notifyDataSetChanged();
+                    }
+                }
+                return true;
+            }
+        });
+    }
+
+    private void initPoiSearchButton(View root){
+        mPoiSearchButton = (Button) root.findViewById(R.id.poi_search_button);
+        mPoiSearchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String keyword=mDestSearchView.getQuery().toString();
+                String city = mToolbar.getTitle().toString();
+                //is the location has failed, there no need and no way to proceed an poi search
+                if (city.equals(getString(R.string.is_locating))) {
+                    Toast.makeText(getContext(), R.string.location_failure, Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mPoiSearch.searchInCity((new PoiCitySearchOption())
+                        .city(city)
+                        .keyword(keyword)
+                        .pageCapacity(10));
+            }
+        });
+    }
+
+    /**
+     * init the list view for show pois by searching,instantiation for listview,arraylist,adapter,and
+     * set it to the listview
+     * @param root
+     */
+    private void initPoiListView(View root) {
+        mSearchResListView = (ListView) root.findViewById(R.id.dest_search_listview);
+        mPoiList = new ArrayList<>();
+        mPoiListAdapter=new SimpleAdapter(getContext(),
+                mPoiList,
+                R.layout.search_poi_list_item,
+                new String[]{POI_NAME,POI_ADDRESS},
+                new int[]{R.id.search_poi_name,R.id.search_poi_address}
+                );
+        mSearchResListView.setAdapter(mPoiListAdapter);
+    }
+
+//    private void initSugSearch() {
+//        SDKInitializer.initialize(getActivity().getApplicationContext());
+//        mSugSearch = SuggestionSearch.newInstance();
+//        mSugSearch.setOnGetSuggestionResultListener(this);
+//    }
+
+    /**
+     * init the PoiSearch, instantiation and set the listener
+     */
+    private void initPoiSearch(){
+        SDKInitializer.initialize(getActivity().getApplicationContext());
+        mPoiSearch=PoiSearch.newInstance();
+        mPoiSearch.setOnGetPoiSearchResultListener(this);
+    }
+
 
     /**
      * A listener for the location result,deal the Location Info here
@@ -118,13 +227,16 @@ public class DestChooseFragment extends Fragment {
 
 
     /**
-     * set some option to the LocationClient,like Mode,or Address needed
+     * set some option to the LocationClient,like Mode,or Address needed,after this function you can
+     * call LocationClient.start() immediately
      */
-    private void initLocationOption() {
+    private void initLocationFunc() {
+        mLocationClient = new LocationClient(getActivity().getApplicationContext());
         LocationClientOption locationClientOption = new LocationClientOption();
         locationClientOption.setLocationMode(LocationClientOption.LocationMode.Battery_Saving);
         locationClientOption.setIsNeedAddress(true);
         mLocationClient.setLocOption(locationClientOption);
+        mLocationClient.registerLocationListener(myLocationListener);
     }
 
     /**
@@ -173,6 +285,49 @@ public class DestChooseFragment extends Fragment {
             if (cityName != null && !cityName.equals("")) {
                 setToolBarTitle(cityName);
             }
+        }
+    }
+
+    @Override
+    public void onGetPoiResult(PoiResult poiResult) {
+        if (poiResult == null || poiResult.getAllPoi() == null) {
+            return;
+        }
+        if (mPoiList == null) mPoiList = new ArrayList<>();
+        mPoiList.clear();
+        new PreparePoiDataTask().execute(poiResult.getAllPoi());
+    }
+
+    @Override
+    public void onGetPoiDetailResult(PoiDetailResult poiDetailResult) {
+
+    }
+
+    private class PreparePoiDataTask extends AsyncTask<List<PoiInfo>,Void,Void>{
+
+        @Override
+        protected Void doInBackground(List<PoiInfo>... params) {
+            List<PoiInfo> pois=params[0];
+            for (PoiInfo info: pois) {
+                if (info.type== PoiInfo.POITYPE.BUS_LINE||info.type==PoiInfo.POITYPE.SUBWAY_LINE){
+                    continue;
+                }
+                Map<String, String> map = new HashMap<>();
+                if (info.name != null&&info.address!=null) {
+                    map.put(POI_NAME,info.name);
+                    map.put(POI_ADDRESS, info.address);
+                }
+                mPoiList.add(map);
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            //when user delete the input every quickly, there will be some delay,and when user delete
+            //all the input, but there still Pois displayed in the list watch is inappropriate
+            if (mDestSearchView.getQuery().length()==0) mPoiList.clear();
+            mPoiListAdapter.notifyDataSetChanged();
         }
     }
 
